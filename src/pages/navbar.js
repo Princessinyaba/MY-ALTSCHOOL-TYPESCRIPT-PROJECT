593fdf55a27abf7e47dd12d4718963285ff5e245
Princess Favour  Inyaba <princessphil99@gmail.com>
2024-08-14 16:05:23 +0300

    still on the bootstrap linking

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,46 +1,58 @@
 import { useRouter } from 'next/router';
 import { FaGoogle } from 'react-icons/fa'; // Import the Font Awesome Google icon
 import Link from 'next/link'; // Import Link from next
-import '../style/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS bundle (with Popper.js)
+import { useEffect } from 'react'; // Import useEffect for dynamic import of Bootstrap JS
+import '../style/styles.css';
 
 const Navbar = () => {
-    return (
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <div className="navbar-brand" href="#">URLScissors</div>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link href="/" passHref>
-                  <div className="nav-link">Home</div>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/about" passHref>
-                  <div className="nav-link">About Us</div>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/pricing" passHref>
-                  <div className="nav-link">API Pricing</div>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/register" passHref>
-                  <div className="nav-link">Register</div>
-                </Link>
-              </li>
-              
-            </ul>
-          </div>
+
+  useEffect(() => {
+    // Dynamically import Bootstrap JS for client-side only
+    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+  }, []);
+
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <Link href="/" passHref>
+          <div className="navbar-brand">URLScissors</div>
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link href="/" passHref>
+                <div className="nav-link">Home</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/about" passHref>
+                <div className="nav-link">About Us</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/pricing" passHref>
+                <div className="nav-link">API Pricing</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/register" passHref>
+                <div className="nav-link">Register</div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/shorten" passHref>
+                <div className="nav-link">ShortCodeRedirect</div>
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
