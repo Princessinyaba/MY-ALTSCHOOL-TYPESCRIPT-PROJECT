f2b53a6093f6b6a888d8943b4bca45a5706938be
Princess Favour Inyaba <princessphil99@gmail.com>
2024-08-14 17:47:59 +0300

    Updated firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,7 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArEQ389jqGjVS6vye3R6rC3etQcupSFWc",
+ apiKey: "AIzaSyArEQ389jqGjVS6vye3R6rC3etQcupSFWc",
   authDomain: "scissor-787b7.firebaseapp.com",
   projectId: "scissor-787b7",
   storageBucket: "scissor-787b7.appspot.com",
